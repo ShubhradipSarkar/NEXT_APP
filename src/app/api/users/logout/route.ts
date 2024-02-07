893e7ddc03d490e3566dd52c41787b92356e770c
shubhradip <shubhradipsarkar@gmail.com>
2024-02-07 15:19:08 +0530

    logout production

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -6,10 +6,10 @@ export async function GET() {
             message: "Logged out",
             success: true,
         })
-        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)});
+        response.cookies.set("token", "", {httpOnly: true});
 
         return response;
-        
+
     } catch (error: any) {
         return NextResponse.json({error: error.message},
             {status: 500});
