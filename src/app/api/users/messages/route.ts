98cc2f6af4286323d9e0a2e394f7b26572e509e0
shubhradip <shubhradipsarkar@gmail.com>
2024-02-08 19:52:26 +0530

    contact form

---
+++ src/app/api/users/messages/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({
+            message: "Logged out",
+            success: true,
+        })
+        response.cookies.delete("token");
+
+        return response;
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+            {status: 500});
+    }
+    
+}
