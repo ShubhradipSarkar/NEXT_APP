a37c5256dd6da36d89ac45b3a427e4d71ce3ef29
shubhradip <shubhradipsarkar@gmail.com>
2024-02-09 22:35:55 +0530

    keno vercel e asche na new manush

---
+++ src/app/testadmin/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import axios from 'axios';
+import React, { useEffect } from 'react'
+
+
+export default function Test() {
+    useEffect(() => {
+        const TT = async() => {
+            const k=await axios.get("/api/users/members");
+            console.log(k);
+        }
+        TT();
+    }, []);
+    return (
+        <div>
+            Nothing
+        </div>
+    )
+}
