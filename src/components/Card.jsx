45e8d80518a55a566aa7210f1f07afea3c9ccfcf
shubhradip <shubhradipsarkar@gmail.com>
2024-02-09 17:36:15 +0530

    setting a

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -84,7 +84,7 @@ export default function Card_({userId, username, email, admin}) {
         <center>
         <CardFooter className="flex justify-between">
             <center><Button variant="outline" size="default">View </Button>
-            {isadmin && !admin && <Button variant="default" size="default" onClick={makeAdmin}>Make Admin </Button>}
+            {isadmin && !admin && <Button variant="default" size="default" onClick={makeAdmin}>Appoint Admin </Button>}
             
             </center>
             
