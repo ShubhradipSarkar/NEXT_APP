8e597b40a346920c656b2026a443ac47615cebc6
shubhradip <shubhradipsarkar@gmail.com>
2024-02-11 11:13:08 +0530

    logout o work korbe

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,5 +1,17 @@
 import { NextResponse } from "next/server";
 
+export async function POST() {
+    try {
+        
+
+        return NextResponse.json({what:"what are you trying to do man"});
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+            {status: 500});
+    }
+    
+}
 export async function GET() {
     try {
         const response = NextResponse.json({
