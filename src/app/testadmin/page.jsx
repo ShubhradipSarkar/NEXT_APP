45dc40b0003a2485fc279b0bb4c3d311e09ce5e0
shubhradip <shubhradipsarkar@gmail.com>
2024-02-11 09:34:25 +0530

    fetch

--- src/app/testadmin/page.jsx
+++ src/app/testadmin/page.jsx
@@ -4,11 +4,13 @@ import axios from 'axios';
 import React, { useEffect } from 'react'
 
 
+
 export default function Test() {
+  //const data = await getData()
     useEffect(() => {
         const TT = async() => {
-            const k=await axios.get("/api/users/members");
-            console.log(k);
+            const k=await fetch("/api/users/members");
+            console.log(await k.json());
         }
         TT();
     }, []);
