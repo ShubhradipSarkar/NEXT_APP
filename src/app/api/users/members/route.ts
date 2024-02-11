2c9d9e801ea10718e65f46fb49116b2761faec5a
shubhradip <shubhradipsarkar@gmail.com>
2024-02-11 10:33:05 +0530

    post member

--- src/app/api/users/members/route.ts
+++ src/app/api/users/members/route.ts
@@ -5,7 +5,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 connect()
 
-export async function GET(request: NextRequest) {
+// export async function POST(request: NextRequest){
+//     try {
+//         const reqBody = await request.json()
+//         const {name, mobile, subject, message} = reqBody
+
+//         // Save Message
+//         const NewMessage = new Messages({
+//             name, 
+//             mobile,
+//             subject,
+//             message,
+//         })
+//         await NewMessage.save();
+
+//         return NextResponse.json({
+//             message: "Message Sent",
+//             success: true,
+//         })
+
+//     } catch (error: any) {
+//         return NextResponse.json({error: error.message},{status: 500})
+//     }
+// }
+
+export async function POST(request: NextRequest) {
     try {
         const member = await Member.find({});
         console.log(member);
